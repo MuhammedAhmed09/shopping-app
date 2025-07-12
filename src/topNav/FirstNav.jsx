f46994aced9fffc4a87e6eb42361635c33a37be2
[Muhammed] <[nameismilo2@gmail.com]>
2025-07-12 19:38:44 +0300

    finish nav

--- src/topNav/FirstNav.jsx
+++ src/topNav/FirstNav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FirstNav = () => {
   return (
-    <div className='flex justify-center bg-rose-600'>
+    <div className='flex justify-center w-full bg-rose-600 shadow-rose-200 shadow-2xl'>
         <h4 className='py-3 text-gray-100'>Free shipping on orders over 1999EGP</h4>
     </div>
   )
--- src/topNav/MainNav.jsx
+++ src/topNav/MainNav.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom';
 
 //ICONS
 import { CiShoppingCart, CiUser } from 'react-icons/ci';
-
-
+import { LiaBarsSolid } from 'react-icons/lia';
 
 const MainNav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
+
     const navItems = [
         {id: 0, path: '/', page: 'Home'},
         {id: 1, path: '/products', page: 'Products'},
@@ -14,28 +16,64 @@ const MainNav = () => {
         {id: 3, path: '/contact-us', page: 'Contact Us'}
     ];
 
-    const location = useLocation();
+    const toggleMenu = () => {
+        setIsOpen(prev => !prev);
+    }
+    console.log(isOpen);
+    
+    
 
   return (
-    <nav className='p-20 border-b border-gray-200 flex items-center justify-between'>
-        <div className='flex items-center '>
-            <Link to={'/'}><h2 className='font-bold text-3xl'>LUNA</h2></Link>
+    <nav className='py-20 px-[10%] border-b border-gray-200 flex items-center justify-between relative'>
+        <div className='flex justify-between w-7/12 flex-row-reverse lg:justify-start lg:w-10/12 lg:flex-row items-center'>
             
-            <ul className='flex justify-around w-2xl'>
+            <Link to='/' className='font-bold text-3xl text-rose-600'>LUNA</Link>
+            
+            <ul className={'hidden lg:flex justify-around w-2xl'}>
                 {navItems.map((item) => (
-                    <li key={item.id} className={`${item.path == location.pathname ? 'border-b hover:border-b-2 border-rose-400 font-semibold': 'text-gray-600 hover:text-gray-950 hover:border-b border-rose-400'}, text-lg`}>
-                        <Link to={item.path}>
-                            <h4>{item.page}</h4>
+                    <li key={item.id}>
+                        <Link
+                            to={item.path}
+                            className={`transition-all duration-200 text-lg ${
+                                item.path == location.pathname 
+                                ?'border-b-2 border-rose-600 text-rose-600 font-semibold'
+                                :'text-gray-600 hover:text-gray-900 hover:border-b border-rose-400'
+                            }`}
+                        >
+                            {item.page}
                         </Link>
                     </li>
                 ))}
             </ul>
+
+            <button onClick={toggleMenu} className='lg:hidden text-3xl text-gray-700 focus:outline-none' aria-label='Toggle menu'>
+                <LiaBarsSolid />
+            </button>
         </div>
 
-        <ul className='flex justify-around w-1/12'>
-            <Link to={'profile'} className='text-3xl hover:text-4xl duration-600 transition-transform'><i><CiUser /></i></Link>
-            <Link to={'cart'} className='text-3xl  duration-300'><i><CiShoppingCart /></i></Link>
-        </ul>
+        <div className='text-3xl flex justify-between w-1/12'>
+            <Link to={'profile'} aria-label='Profile'><i><CiUser /></i></Link>
+            <Link to={'cart'} aria-label='Cart'><i><CiShoppingCart /></i></Link>
+        </div>
+
+        {/* MOBILE menu */}
+        {isOpen && (
+            <ul className='absolute top-full left-0  w-full bg-rose-600 text-white flex flex-col items-center gap-6 py-6 lg:hidden z-99'>
+                {navItems.map((item) => (
+                    <li key={item.id} >
+                        <Link
+                            to={item.path}
+                            className={`text-lg ${
+                                item.path === location.pathname ? 'font-bold underline' : ''
+                            }`}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            {item.page}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        )}
     </nav>
   )
 }
